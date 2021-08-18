efd7108281eb95e8fbfb08ceb9dcba3827f050e3
z-memory <dont@forgetmy.email>
2021-08-18 21:40:40 +0200

    init thing

---
+++ index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app
+  .route("/")
+  .get((req, res) => {
+    res.send("uhhhh... " + Math.random() + " " + Date.now());
+  })
+  .put((req, res) => {
+     res.sendFile(path.join(process.cwd(),'put.html'))
+  })
+  .delete((req, res) => {
+    res.json({"good" : "yep"})
+  })
+
+app
+  .get("/test-ejs", (req, res) => {
+    res.render('pages/index');
+  })
+  
+var server = app.listen(3000,function(){
+  console.log('server is now running on http://localhost:3000');
+});
